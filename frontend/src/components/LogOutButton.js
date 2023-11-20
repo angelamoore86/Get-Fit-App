62e10850ce625fbcafc8d72a05a319e0fa37722e
angelamoore86 <122281605+angelamoore86@users.noreply.github.com>
2023-11-20 10:05:18 -0330

    Adding code for private route and adding a logout button

---
+++ frontend/src/components/LogOutButton.js
@@ -0,0 +1,15 @@
+import { useNavigate } from "react-router-dom";
+
+
+export const LogOutButton = () => {
+    const navigate = useNavigate();
+
+    const handleLogOut = () => {
+        localStorage.removeItem('token');
+        navigate('/login');
+    };
+
+    return (
+        <button onClick={handleLogOut}>Log Out</button>
+    );
+};
