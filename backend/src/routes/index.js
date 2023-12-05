2b3fea5d2f364d2b263bc3b9ff125f4c53aa66c8
angelamoore86 <122281605+angelamoore86@users.noreply.github.com>
2023-12-05 13:15:07 -0330

    Edited index.js to remove goal route

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -4,7 +4,6 @@ import { googleOauthCallbackRoute } from "./googleOauthCallbackRoute.js";
 import { updateProfileRoute } from "./updateProfileRoute.js";
 import { getProfileRoute } from "./getProfileRoute.js";
 import { updateGoalRoute } from "./updateGoalRoute.js";
-import { getGoalRoute } from "./getGoalRoute.js";
 
 export const routes = [
     testRoute,
@@ -12,6 +11,5 @@ export const routes = [
     googleOauthCallbackRoute,
     getProfileRoute,
     updateProfileRoute,
-    getGoalRoute,
     updateGoalRoute,
 ];
